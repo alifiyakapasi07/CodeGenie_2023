16796fa61fd83039319e6221de25e7b6ffde6fc5
alifiyakapasi07 <103169791+alifiyakapasi07@users.noreply.github.com>
2023-02-25 19:22:35 +0530

    Create StudentScreener.py

--- problem-solution/StudentScreener.py
+++ problem-solution/StudentScreener.py
@@ -1,38 +1,24 @@
-def student_screener(students, percentile_criteria):
-    #percentile score for each student
-    for student in students:
-        percentile_score = (student['marks_obtained'] / student['total_marks']) * 100
+def find_eligible_students(students, marks_obtained, total_marks, exam_result, eligibility_percentile):
 
-        # Add the percentile score to the student's data
-        student['percentile_score'] = percentile_score
-
-    # Sort students by their percentile score in descending order
-    sorted_students = sorted(students, key=lambda x: x['percentile_score'], reverse=True)
-
-    # cutoff percentile score based on the percentile criteria
-    cutoff_percentile_score = sorted_students[int(len(sorted_students) * percentile_criteria / 100)]['percentile_score']
-
-    # Filter the eligible students based on the cutoff percentile score
-    eligible_students = [student for student in sorted_students if student['percentile_score'] >= cutoff_percentile_score and student['result'] == 'Passed']
-
+    # Calculate the percentile rank of each student
+    percentile_ranks = [100*(sum([1 for j in marks_obtained if j<=marks_obtained[i]])-1)/(len(marks_obtained)-1) for i in range(len(students))]
+    
+    # Find the cutoff percentile based on the eligibility percentile
+    cutoff_percentile = max(percentile_ranks) - (max(percentile_ranks) - min(percentile_ranks)) * eligibility_percentile/100
+    
+    # Create a list of eligible students
+    eligible_students = [students[i] for i in range(len(students)) if percentile_ranks[i] >= cutoff_percentile and exam_result[i]=="Passed"]
+    
+    # Return the list of eligible students
     return eligible_students
 
-  #Sample data
-students = [
-    {'name': 'Kartik', 'marks_obtained': 800, 'total_marks': 1000, 'result': 'Passed'},
-    {'name': 'Devang', 'marks_obtained': 300, 'total_marks': 1000, 'result': 'Failed'},
-    {'name': 'Pari', 'marks_obtained': 750, 'total_marks': 1000, 'result': 'Passed'},
-    {'name': 'Ketan', 'marks_obtained': 760, 'total_marks': 1000, 'result': 'Failed'},
-    {'name': 'Sheetal', 'marks_obtained': 680, 'total_marks': 1000, 'result': 'Passed'},
-     {'name': 'Darshana', 'marks_obtained': 790, 'total_marks': 1000, 'result': 'Passed'},
-    {'name': 'Mohan', 'marks_obtained': 640, 'total_marks': 1000, 'result': 'Passed'}
-]
+# Example usage
 
-percentile_criteria = 50
+students = ["Kartik" ,"Devang", "Pari", "Ketan", "Sheetal", "Darshana", "Mohan"]
+marks_obtained = [800, 300, 750, 760, 680, 790, 640]
+total_marks = 1000
+exam_result = ["Passed", "Failed", "Passed", "Failed", "Passed", "Passed", "Passed"]
+eligibility_percentile = 50
 
-# Find eligible students
-eligible_students = student_screener(students, percentile_criteria)
-
-# Print the eligible students
-for student in eligible_students:
-    print(student['name'])
+eligible_students = find_eligible_students(students, marks_obtained, total_marks, exam_result, eligibility_percentile)
+print("Eligible students:", eligible_students)
